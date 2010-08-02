b42171594409a9aba0d793f2d59a9711812b6ce0
Gilles Crofils (Guignes) <gilles@secondbureau.com>
2010-08-02 21:50:21 +0200

    bugfile word missing

--- lib/server/mysql.rb
+++ lib/server/mysql.rb
@@ -9,7 +9,7 @@ class Mysql
   def create_database mysql_root_password
     unless mysql_root_password.nil?
       conf = ActiveRecord::Base.configurations
-      cmd = " CREATE DATABASE #{conf[:database]}  SET utf8 COLLATE utf8_bin;
+      cmd = " CREATE DATABASE #{conf[:database]} CHARACTER SET utf8 COLLATE utf8_bin;
             CREATE USER '#{conf[:username]}'@'localhost' IDENTIFIED BY '#{conf[:password]}';
             GRANT ALL PRIVILEGES on #{conf[:database]}.* to #{conf[:username]}@localhost IDENTIFIED BY '#{conf[:password]}'"
       tf = Tempfile.new('mysql_')
