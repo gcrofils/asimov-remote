d8ff0d1b03ee6a1b4ef97a42f3a1dc7c05a5ae7b
Gilles Crofils <gilles@secondbureau.com>
2010-08-05 17:58:18 +0200

    rm comments

--- lib/commands/load_magento_data.rb
+++ lib/commands/load_magento_data.rb
@@ -1,7 +1,5 @@
 require 'server'
 require 'settings'
-#Magento.new.load_data
-options = {:parent_id => 2, :name => 'tuti', 'is_active' => 1, 'default_sort_by' => 'name', 'available_sort_by' => {'ArrayOfString' => 'name'}}
-Mage::Api.new.create_category(options)
+Magento.new.load_data
 
 puts "load_magento_data"
