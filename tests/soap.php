0f3d1b9f028746e67643b2e95993a81eec660432
Gilles Crofils <gilles@secondbureau.com>
2010-08-05 17:53:27 +0200

    add php soap test

---
+++ tests/soap.php
@@ -0,0 +1,10 @@
+client = new SoapClient('http://delhaye.milizone.com/api/?wsdl');
+// If soap isn't default use this link instead
+// http://youmagentohost/api/soap/?wsdl
+ 
+// If somestuff requires api authentification,
+// we should get session token
+$session = $client->login('admin', 'secret09');
+ 
+$result = $client->call($session, 'create.directory', array (2, array ('name' => 'titi')));
+print_r ($result);
