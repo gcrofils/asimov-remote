44faa23e6220ad04ea8b5dfc50e7bf3318bcd241
Gilles Crofils <gilles@secondbureau.com>
2011-01-23 00:13:49 +0800

    #374

--- lib/server/bind.rb
+++ lib/server/bind.rb
@@ -3,14 +3,11 @@ class Bind < Server::Base
 
   def default_settings
   {
-          :bind_conf_path => "/usr/local/bind9/etc/",
-          :bind_conf_template => 'named.conf'
+          :bind_conf_path => "/chroot/",
   }
   end
 
   def setup
-     FileUtils.cp File.join(ASIMOV_ROOT, 'etc', configuration[:bind_conf_template]), configuration[:bind_conf_path]
-     FileUtils.cp_r File.join(ASIMOV_ROOT, 'etc', 'namedb'), configuration[:bind_conf_path]
-     FileUtils.ln_s File.join(ASIMOV_ROOT, 'etc', 'namedb'), '/etc/namedb'
+     FileUtils.cp_r File.join(ASIMOV_ROOT, 'etc', 'named'), configuration[:bind_conf_path]
   end
 end
