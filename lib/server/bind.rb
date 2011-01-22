6bff6aca412b7ac984675e1d1cb368e080bd9112
Gilles Crofils <gilles@secondbureau.com>
2011-01-22 22:43:23 +0800

    #374

--- lib/server/bind.rb
+++ lib/server/bind.rb
@@ -10,5 +10,7 @@ class Bind < Server::Base
 
   def setup
      FileUtils.cp File.join(ASIMOV_ROOT, 'etc', configuration[:bind_conf_template]), configuration[:bind_conf_path]
+     FileUtils.cp_r File.join(ASIMOV_ROOT, 'etc', 'namedb'), configuration[:bind_conf_path]
+     FileUtils.ln_s File.join(ASIMOV_ROOT, 'etc', 'namedb'), '/etc/namedb'
   end
 end
