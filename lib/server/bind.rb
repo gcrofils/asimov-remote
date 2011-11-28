c028340b50283a4ba3bce1a60d06f93c3e26f15b
Gilles Crofils <gilles@secondbureau.com>
2011-11-28 18:33:20 +0800

    added updateGeoIp DB cron task

--- lib/server/bind.rb
+++ lib/server/bind.rb
@@ -9,14 +9,16 @@ class Bind < Server::Base
 
   def setup
      FileUtils.cp_r File.join(ASIMOV_ROOT, 'etc', 'named'), configuration[:bind_conf_path]
+     FileUtils.cp File.join(ASIMOV_ROOT, 'etc', 'cron', 'updateGeoIp'), '/etc/cron.monthly/'
      %w[perms start].each do |ext|
        FileUtils.mv File.join(configuration[:bind_conf_path], 'named', "named.#{ext}"), File.join(configuration[:bind_conf_path], "named.#{ext}")
      end
      FileUtils.ln_sf File.join(configuration[:bind_conf_path], 'named', 'etc', 'rndc.conf'), '/usr/local/bind9/etc/rndc.conf'
      FileUtils.ln_sf File.join(configuration[:bind_conf_path], 'named', 'etc', 'rndc.conf'), '/etc/rndc.conf'
-     FileUtils.ln_sf "/chroot/named/named", "/etc/init.d/named" 
+     FileUtils.ln_sf "/chroot/named/named", "/etc/init.d/named"
      geoIP = File.join('/', 'chroot', 'named', 'usr', 'local', 'share', 'GeoIP')
      FileUtils.mkdir_p geoIP
+     system ('sh /etc/cron.monthly/updateGeoIp')
      FileUtils.cp '/usr/local/share/GeoIP/GeoIP.dat', File.join(geoIP, 'GeoIP.dat')
      system ('sh /chroot/named.perms')
   end
